1ec7b5efc110d2088ac79abd45ba78422a4d1ee5
webconfss <101816572+webconfss@users.noreply.github.com>
2022-04-16 05:36:42 +0430

    Update des_c.py

--- core/lib/mschap/des_c.py
+++ core/lib/mschap/des_c.py
@@ -1,3 +1,5 @@
+
+#coding:utf-8
 # This file is part of 'NTLM Authorization Proxy Server'
 # Copyright 2001 Dmitry A. Rozmanov <dima@xenon.spb.ru>
 #
@@ -19,7 +21,7 @@
 
 from U32 import U32
 
-# Что бы это значило?
+# Г—ГІГ® ГЎГ» ГЅГІГ® Г§Г­Г Г·ГЁГ«Г®?
 #typedef unsigned char des_cblock[8];
 #define HDRSIZE 4
 
